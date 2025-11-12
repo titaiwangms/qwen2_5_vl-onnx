982f7181f9ace25b34754a06a330027bc82ac08b
Xavier Dupré <xadupre@microsoft.com>
2025-11-12 19:18:22 +0000

    discrepancies with true images

--- builder.py
+++ builder.py
@@ -12,12 +12,23 @@ from torch.onnx._internal.exporter import _testing
 def build_vision(args):
     # NOTE: Shape: [total_patches_across_all_images, patch_volume]
     # This is to accomodate to video input where multiple images are passed in a batch.
-    pixel_values = torch.randn((14308, 1176), dtype=torch.float32)
-    # Scale the values to the range [-1, 0.95] to fit actual values we observed in the example.
-    pixel_values = pixel_values * (0.95 - (-1)) + (-1)
-    pixel_values = pixel_values.to(args.precision).to(args.execution_provider.replace("dml", "cuda"))
 
-    grid_thw = torch.tensor([[1, 98, 146]], dtype=torch.int64).to(args.execution_provider.replace("dml", "cuda"))
+
+    if False:
+        pixel_values = torch.randn((1292, 1176), dtype=torch.float32)
+        # Scale the values to the range [-1, 0.95] to fit actual values we observed in the example.
+        pixel_values = pixel_values * (0.95 - (-1)) + (-1)
+        grid_thw = torch.tensor([[1, 34, 38]], dtype=torch.int64)
+    else:
+        from onnx_diagnostic.helpers.mini_onnx_builder import (
+            create_input_tensors_from_onnx_model,
+        )
+        inputs = create_input_tensors_from_onnx_model("data/get_image_features.inputs.onnx")
+        pixel_values = inputs["pixel_values"].to(torch.float32)
+        grid_thw = inputs["image_grid_thw"]
+
+    pixel_values = pixel_values.to(args.precision).to(args.execution_provider.replace("dml", "cuda"))
+    grid_thw = grid_thw.to(args.execution_provider.replace("dml", "cuda"))
 
     # Dynamo export
     dummy_inputs = {
@@ -26,7 +37,7 @@ def build_vision(args):
     }
     dynamic_shapes = {
         "pixel_values": {0: "num_patches"},
-        "image_grid_thw": None
+        "image_grid_thw": {}
     }
 
     # NOTE: hack to image model export
@@ -249,6 +260,8 @@ if __name__ == "__main__":
         if args.part == "embedding":
             build_embedding(args)
         elif args.part == "vision":
+            # cp modeling_code/modeling_qwen2_5_vl.py ../transformers/src/transformers/models/qwen2_5_vl/
+            # CUDA_VISIBLE_DEVICES=4,5,6 python builder.py -i Qwen/Qwen2.5-VL-7B-Instruct -o qwen_25_vl_bf16_vision -p bf16 -e cuda --part vision
             build_vision(args)
         else:
             build_embedding(args)
